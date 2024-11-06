7f7c1fb3e55c732dcb5b4ec2065d071fd47c9979
ZDSJdeJT <2240883644@qq.com>
2024-11-06 23:14:08 +0800

    updated styles

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import MinesweeperGame from '@/components/MinesweeperGame.vue'
 </script>
 
 <template>
-  <div class="w-full p-8 flex flex-col justify-center items-center gap-8">
+  <div class="w-full p-8 flex flex-col justify-center items-center gap-6">
     <ModeToggle />
     <MinesweeperGame />
   </div>
